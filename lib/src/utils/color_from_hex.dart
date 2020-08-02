fc0e97d11865858ee495f6afea164b161249f36b
marioki <mariokirven.mk@gmail.com>
2020-08-02 09:58:06 -0500

    getColorFromHex null safety

--- lib/src/utils/color_from_hex.dart
+++ lib/src/utils/color_from_hex.dart
@@ -3,13 +3,17 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 Color getColorFromHex(String hexColor) {
-  hexColor = hexColor.replaceAll("#", "");
-  if (hexColor.length == 6) {
-    hexColor = "FF" + hexColor;
-  }
-  if (hexColor.length == 8) {
-    return Color(int.parse("0x$hexColor"));
+  if (hexColor != null) {
+    hexColor = hexColor.replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    if (hexColor.length == 8) {
+      return Color(int.parse("0x$hexColor"));
+    } else {
+      return Colors.grey;
+    }
   } else {
-    return Colors.white;
+    return Colors.grey;
   }
 }
