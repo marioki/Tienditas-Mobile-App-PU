efae4f83ecea2ffb1e2172b2a01f605217c9762b
marioki <mariokirven.mk@gmail.com>
2020-08-12 13:36:41 -0500

    simplified DeliveyOptionsPAge list view

--- lib/src/pages/checkout_sequence/delivery_options_page.dart
+++ lib/src/pages/checkout_sequence/delivery_options_page.dart
@@ -1,6 +1,6 @@
 import 'package:app_tiendita/src/modelos/delivery_options_response.dart';
-import 'package:app_tiendita/src/pages/checkout_sequence/escoger_direcciones_page.dart';
 import 'package:app_tiendita/src/providers/store_delivery_options_provider.dart';
+import 'package:app_tiendita/src/state_providers/user_cart_state.dart';
 import 'package:app_tiendita/src/tienditas_themes/my_themes.dart';
 import 'package:app_tiendita/src/widgets/alert_dialogs/delivery_alert_dialog.dart';
 import 'package:flutter/material.dart';
@@ -14,118 +14,107 @@ class _DeliveryOptionsPageState extends State<DeliveryOptionsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: grisClaroTema,
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(100),
-          child: AppBar(
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(35),
-                    bottomRight: Radius.circular(35))),
-            centerTitle: true,
-            backgroundColor: azulTema,
-            title: Text(
-              'Delivery',
-              style: appBarStyle,
-            ),
+      backgroundColor: grisClaroTema,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: AppBar(
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(35),
+                  bottomRight: Radius.circular(35))),
+          centerTitle: true,
+          backgroundColor: azulTema,
+          title: Text(
+            'Delivery',
+            style: appBarStyle,
           ),
         ),
-        body: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: 8,
-              width: double.infinity,
-            ),
-            Text(
-              'Escoge la opción de envío para tus pedidos...',
-              textAlign: TextAlign.center,
-              overflow: TextOverflow.clip,
-            ),
-            SizedBox(
-              height: 8,
-              width: double.infinity,
-            ),
-            Container(
-              height: 600,
-              child: FutureBuilder(
-                //Todo el metodo de getStoreDeliveryOptions retorne una lista de listas
-                future: DeliveryOptionsProvider()
-                    .getStoreDeliveryOptions(context, '@razer'),
-                builder:
-                    (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                  if (snapshot.hasData) {
-                    return ListView.builder(itemCount: 1,shrinkWrap: true,itemBuilder: (context, index) {
-                      DeliveryOptionsResponse deliveryOptionsResponse = snapshot.data;
-                      StoreDelliveryInfo storeDelliveryInfo = deliveryOptionsResponse.body.store;
-                      return getDeliveryOptionsWidget(context, storeDelliveryInfo);
-                    });
-                  } else {
-                    return Container(
-                      height: 400,
-                      child: Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                    );
-                  }
-                },
-              ),
-            )
-          ],
-        ),
-        bottomSheet: Container(
-          padding: EdgeInsets.all(16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'COSTO DE ENVÍO',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: grizSubtitulo,
+      ),
+      body: FutureBuilder(
+        //Todo el metodo de getStoreDeliveryOptions retorne una lista de listas
+        future: DeliveryOptionsProvider()
+            .getStoreDeliveryOptions(context, '@razer'),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.separated(
+                separatorBuilder: (context, index) => Divider(
+                      color: Colors.black,
+                      thickness: 2,
                     ),
-                  ),
-                  Text(
-                    '\$100.00',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Nunito',
-                      color: azulTema,
-                    ),
-                  ),
-                ],
-              ),
-              FlatButton(
-                child: Text(
-                  'SIGUIENTE',
-                  style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.white,
-                      fontFamily: 'Nunito',
-                      fontWeight: FontWeight.bold),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => EscogerDirecciones(),
-                    ),
+                itemCount: 1,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  DeliveryOptionsResponse deliveryOptionsResponse =
+                      snapshot.data;
+                  StoreDelliveryInfo storeDelliveryInfo =
+                      deliveryOptionsResponse.body.store;
+                  return ListTile(
+                    title: Text(storeDelliveryInfo.deliveryOptions[0].name),
                   );
-                },
-                color: azulTema,
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 24),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(35)),
+                });
+          } else {
+            return Container(
+              height: 400,
+              child: Center(
+                child: CircularProgressIndicator(),
               ),
-            ],
-          ),
-        ));
+            );
+          }
+        },
+      ),
+      //      bottomSheet: Container(
+//        padding: EdgeInsets.all(16),
+//        child: Row(
+//          mainAxisAlignment: MainAxisAlignment.spaceAround,
+//          children: <Widget>[
+//            Column(
+//              mainAxisSize: MainAxisSize.min,
+//              crossAxisAlignment: CrossAxisAlignment.start,
+//              children: <Widget>[
+//                Text(
+//                  'COSTO DE ENVÍO',
+//                  style: TextStyle(
+//                    fontWeight: FontWeight.bold,
+//                    color: grizSubtitulo,
+//                  ),
+//                ),
+//                Text(
+//                  '\$100.00',
+//                  style: TextStyle(
+//                    fontWeight: FontWeight.bold,
+//                    fontFamily: 'Nunito',
+//                    color: azulTema,
+//                  ),
+//                ),
+//              ],
+//            ),
+//            FlatButton(
+//              child: Text(
+//                'SIGUIENTE',
+//                style: TextStyle(
+//                    fontSize: 12,
+//                    color: Colors.white,
+//                    fontFamily: 'Nunito',
+//                    fontWeight: FontWeight.bold),
+//              ),
+//              onPressed: () {
+//                Navigator.push(
+//                  context,
+//                  MaterialPageRoute(
+//                    builder: (context) => EscogerDirecciones(),
+//                  ),
+//                );
+//              },
+//              color: azulTema,
+//              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 24),
+//              shape: RoundedRectangleBorder(
+//                  borderRadius: BorderRadius.circular(35)),
+//            ),
+//          ],
+//        ),
+//      ),
+    );
   }
 
   Widget getDeliveryOptionsWidget(
