15fe41c7f60e45dc01225192bd12f84fcdee2b14
marioki <mariokirven.mk@gmail.com>
2020-08-30 15:31:27 -0500

    Delete PlaceHolder Page

--- lib/src/rutas/rutas.dart
+++ lib/src/rutas/rutas.dart
@@ -2,7 +2,6 @@ import 'package:app_tiendita/src/pages/cart_page.dart';
 import 'package:app_tiendita/src/pages/categories_page.dart';
 import 'package:app_tiendita/src/pages/home_page.dart';
 import 'package:app_tiendita/src/pages/login_page.dart';
-import 'package:app_tiendita/src/pages/place_holder_page.dart';
 import 'package:app_tiendita/src/pages/profile_page.dart';
 import 'package:app_tiendita/src/pages/search_for_store_page.dart';
 import 'package:app_tiendita/src/pages/store_items_page.dart';
@@ -29,7 +28,6 @@ Map<String, WidgetBuilder> getApplicationRoutes(bool loggedIn) {
     'store_items_page': (BuildContext context) => StoreItemsPage(),
     'categories_page': (BuildContext context) => CategoriesPage(),
     'login_page': (BuildContext context) => LoginPage(),
-    'place_holder_page': (BuildContext context) => PlaceHolderPage(),
     'stores_by_category': (BuildContext context) => StoresByCategory(),
     'search_for_store': (BuildContext context) => SearchForStorePage(),
   };
