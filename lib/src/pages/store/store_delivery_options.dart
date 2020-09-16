c4e566f9fd84e38024cd19af1b3b0d5fe312828e
Julian Tuberquia <julian@Julians-MacBook-Pro.local>
2020-09-15 21:25:25 -0500

    create store

--- lib/src/pages/store/store_delivery_options.dart
+++ lib/src/pages/store/store_delivery_options.dart
@@ -35,8 +35,7 @@ class _StoreDeliveryOptionsState extends State<StoreDeliveryOptions> {
             icon: Icon(
               Icons.add
             ),
-            iconSize: 45,
-            color: Colors.lightGreenAccent,
+            iconSize: 40,
             padding: EdgeInsets.only(right: 16.0),
             onPressed: () {
               Navigator.push(
