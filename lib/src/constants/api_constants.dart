9178b1144ba0ed5bab10e122072e2768d8b6f9c2
marioki <mariokirven.mk@gmail.com>
2020-10-11 15:21:08 -0500

    actualizar imagen de tienda

--- lib/src/constants/api_constants.dart
+++ lib/src/constants/api_constants.dart
@@ -1,12 +1,18 @@
+import 'package:flutter/foundation.dart' as Foundation;
+
+const String production =
+    'https://lnw0tmrts2.execute-api.us-east-1.amazonaws.com/prod';
+const String develop =
+    'https://8s27xrnoq8.execute-api.us-east-1.amazonaws.com/dev';
+
 //URL Para el API De Desarrollo
 const String apiDevBaseUrl =
-    'https://aua4psji8k.execute-api.us-east-1.amazonaws.com/dev';
-
+    'https://lnw0tmrts2.execute-api.us-east-1.amazonaws.com/prod';
 
 //URL Para el API De Producción
 //TODO Cambiar a prod en release
 const String baseApiUrl =
-    'https://aua4psji8k.execute-api.us-east-1.amazonaws.com/dev';
+    'https://8s27xrnoq8.execute-api.us-east-1.amazonaws.com/dev';
 
 //URL Imagen Por defecto Cuando se crea un producto nuevo
 const String defaultProductImageURL =
