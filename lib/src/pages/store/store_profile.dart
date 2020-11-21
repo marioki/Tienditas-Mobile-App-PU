2aff9336af2e01aee5df866659623ebd17fd6641
Mario <mmontenegro@consultec-ti.com>
2020-11-20 21:06:20 -0500

    delete

--- lib/src/pages/store/store_profile.dart
+++ lib/src/pages/store/store_profile.dart
@@ -267,30 +267,4 @@ class ActionButton extends StatelessWidget {
       ),
     );
   }
-
-  Widget _buildLoginBtn() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 25.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Pressed SignUp'),
-        padding: EdgeInsets.all(15.0),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30.0),
-        ),
-        color: Colors.white,
-        child: Text(
-          'Registrarme',
-          style: TextStyle(
-            color: azulTema,
-            letterSpacing: 1.5,
-            fontSize: 18.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'Nunito',
-          ),
-        ),
-      ),
-    );
-  }
 }
